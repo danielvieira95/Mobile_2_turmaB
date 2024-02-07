06914bc478df24238e6f3bb0b0188ec3c3a60396
danielvieira95 <engdanielvieira95@gmail.com>
2024-02-06 21:14:46 -0300

    App 2 turma B - dinamico

---
+++ app_aula2_dinamico/android/app/src/main/kotlin/aula2/u1com/example/app_aula2_dinamico/MainActivity.kt
@@ -0,0 +1,6 @@
+package aula2.u1com.example.app_aula2_dinamico
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_aula2_dinamico/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Telaprincipal());
+}
+
+class Telaprincipal extends StatelessWidget {
+  const Telaprincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplicativo Dinâmico"),
+        ),
+        body:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+          Contador(),
+         ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class Contador extends StatefulWidget {
+  const Contador({super.key});
+
+  @override
+  State<Contador> createState() => ContadorState();
+}
+
+class ContadorState extends State<Contador> {
+   int cont = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.blue,
+          height: 150,
+          width: 150,
+           // Child estabelece relação entre o container e o texto
+         child: Text("${cont}",
+         style: TextStyle(fontSize: 30),),
+        ),
+        ElevatedButton(onPressed: (){
+          setState(() {
+            cont++;
+          });
+        }, 
+        child: Text("Contador")),
+      ],
+    );
+  }
+}
